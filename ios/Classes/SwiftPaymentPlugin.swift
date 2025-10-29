4dc10052f9321ddc8b633afe0bf6a0103d816579
Mohamed Ehab <m.ehab@petroapp.com>
2025-10-29 11:34:59 +0300

    Enhance Apple Pay support by updating payment networks configuration in SwiftPaymentPlugin.swift

--- ios/Classes/SwiftPaymentPlugin.swift
+++ ios/Classes/SwiftPaymentPlugin.swift
@@ -118,18 +118,18 @@ public class SwiftPaymentPlugin: NSObject,FlutterPlugin ,SFSafariViewControllerD
 
              let checkoutSettings = OPPCheckoutSettings()
              checkoutSettings.paymentBrands = self.brandsReadyUi;
-             if(self.brandsReadyUi.contains("APPLEPAY")){
+              if(self.brandsReadyUi.contains("APPLEPAY")){
 
                      let paymentRequest = OPPPaymentProvider.paymentRequest(withMerchantIdentifier: self.applePaybundel, countryCode: self.countryCode)
                      paymentRequest.paymentSummaryItems = [PKPaymentSummaryItem(label: self.companyName, amount: NSDecimalNumber(value: self.amount))]
 
-                        paymentRequest.supportedNetworks = [ PKPaymentNetwork.mada ]
-                    //  if #available(iOS 12.1.1, *) {
-                    //  }
-                    //  else {
-                    //      // Fallback on earlier versions
-                    //      paymentRequest.supportedNetworks = [ PKPaymentNetwork.visa, PKPaymentNetwork.masterCard ]
-                    //  }
+                     if #available(iOS 12.1.1, *) {
+                         paymentRequest.supportedNetworks = [ PKPaymentNetwork.mada,PKPaymentNetwork.visa, PKPaymentNetwork.masterCard ]
+                     }
+                     else {
+                         // Fallback on earlier versions
+                         paymentRequest.supportedNetworks = [ PKPaymentNetwork.visa, PKPaymentNetwork.masterCard ]
+                     }
                      checkoutSettings.applePayPaymentRequest = paymentRequest
                     // checkoutSettings.paymentBrands = ["APPLEPAY"]
              }
